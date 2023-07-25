60de966ce0615fafd4d01e10e1cf129697085f90
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-07-24 22:13:09 -0700

    working with Xrm and getContent!

--- tutorial_web_resources/js_wr_tutorial.js
+++ tutorial_web_resources/js_wr_tutorial.js
@@ -1,114 +1,67 @@
-let formContext = null;
-let executionContext = null;
-let studentData = null;
-const apiEndpoint = 'https://cfainternship.api.crm.dynamics.com/api/data/v9.2/in23gl_students';
 
+
+/**
+ * The code `const xrm = { WebApi: Xrm.WebApi };` is creating a constant variable `xrm` and assigning it an object with a
+ * property `WebApi`. The value of the `WebApi` property is `Xrm.WebApi`, which is  a reference to the Xrm library.This
+ * allows the code to access the Web API functionality through the `xrm` object.
+ * */
+const XRM = {
+    WebApi: Xrm.WebApi
+};
+
+const lookUpValues = {};
+/**
+ * The onLoad function retrieves university and student data based on a filter, and then passes the student data to an HTML
+ * web resource control.
+ * @param context - The `context` parameter is the execution context of the form. It provides access to various properties
+ * and methods related to the form and its controls. In this code, it is used to get the form context using the
+ * `getFormContext()` method.
+ */
 function onLoad(context) {
     executionContext = context;
     formContext = context.getFormContext();
     console.log("formContextOnLoad: ", formContext);
 
-    const  universityName = formContext.getAttribute("in23gl_universityname").getValue();
+    const universityName = formContext.getAttribute("in23gl_universityname").getValue();
     const filter = `?$filter=in23gl_universityname eq '${universityName}'`;
-    // filter applies String value of selected university to get the id to create idFilter
+
     Xrm.WebApi.retrieveMultipleRecords("in23gl_university", filter).then (
         function success(universityRows) {
+            lookUpValues.universities = universityRows.entities[0];
             const universityId = universityRows.entities[0].in23gl_universityid;
             const idFilter = `?$filter=_in23gl_university_value eq '${universityId}'`;
-            //idFilter is used to show only students belonging to the selected university
+
             Xrm.WebApi.retrieveMultipleRecords("in23gl_student", idFilter).then(
                 function success(students) {
-                    // perform additional operations on retrieved records
                     studentData = students;
-                    fetchStudents(students);
+                    console.log("STUDENT DATA ONLOAD: ", students)
+                    // Get the HTML web resource control
+                    const htmlControl = formContext.getControl("WebResource_Student");
+                    console.log("HTMLCONTROL: ", htmlControl)
+                    // Use getContentWindow to get the window object of the HTML web resource
+                    htmlControl.getContentWindow().then(
+                        function success(contentWindow) {
+                        // Call the fetchStudents function in the HTML web resource
+                            contentWindow.xrm = XRM;
+                            contentWindow.lookupData = lookUpValues;
+                            contentWindow.fetchStudents(studentData);
+                            console.log('lookUpValues', lookUpValues)
+                            console.log("CNTWIN: ", contentWindow)
+                        },
+                        function error(error) {
+                            console.log(error.message)
+                        });
 
                 },
-                function (error) {
+                function error(error) {
                     console.log(error.message);
-                    // handle error conditions
                 }
             );
         },
-        function (error) {
+        function error(error) {
             console.log(error.message);
-            // handle error conditions
         }
     );
-
-}
-
-function fetchStudents(students) {
-        console.log("StudentData: ", students);
-        // Process the result
-        const table = document.getElementById('studentsTable');
-
-        // Loop through the data and create a table row for each student
-    students.forEach( student => {
-
-        const row = table.insertRow();
-        const idCell = row.insertCell()
-        const firstNameCell = row.insertCell();
-        const lastNameCell = row.insertCell();
-        const dobCell = row.insertCell();
-        const emailCell = row.insertCell();
-        const universityCell = row.insertCell();
-        const actionCell = row.insertCell();
-
-        //access the data from the entities and assign to corresponding cell within the table
-        idCell.textContent = student.in23gl_studentid;
-        firstNameCell.textContent = student.in23gl_firstname;
-        lastNameCell.textContent = student.in23gl_lastname;
-        dobCell.textContent = student.in23gl_dob;
-        emailCell.textContent = student.in23gl_email;
-
-        // Make the cells editable
-        idCell.contentEditable = 'true';
-        firstNameCell.contentEditable = 'true';
-        lastNameCell.contentEditable = 'true';
-        dobCell.contentEditable = 'true';
-        emailCell.contentEditable = 'true';
-        universityCell.contentEditable = 'true';
-
-        // Create update button
-        const updateButton = document.createElement('button');
-        updateButton.textContent = 'Update';
-        updateButton.onclick = () => {
-            // Handle update operation
-            console.log('Update button clicked for student', student);
-        };
-
-        // Create delete button
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.onclick = () => {
-            // Handle delete operation
-            deleteRow(deleteButton);
-        };
-
-        // Add buttons to action cell
-        actionCell.appendChild(updateButton);
-        actionCell.appendChild(deleteButton);
-    });
-}
-
-
-
-function createStudent() {
-
-}
-
-function updateStudent(id) {
-
-}
-function deleteRow(button) {
-    // Get the row of the button
-    const row = button.parentNode.parentNode;
-    // TODO: remove html row after success on removal from db
-    const id = row.cells[0].textContent;
-    console.log("ID: ", id);
-    // Remove record from dataverse table
-    // on success remove record from table
-    row.parentNode.removeChild(row);
 }
 
 
